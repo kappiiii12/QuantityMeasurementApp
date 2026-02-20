1ce0b3e0cdeea0c188081386d9e3bd84e12e149c
kapil kumar <kapil.kumar1_cs22@gla.ac.in>
2026-02-20 23:06:45 +0530

    [Kapil Kumar] Added : feature/UC1-FeetEquality

---
+++ app/src/main/java/com/quantitymeasurementapp/app/QuantityMeasurementApp.java
@@ -0,0 +1,25 @@
+package com.quantitymeasurementapp.app;
+
+/**
+ * Hello world!
+ */
+public class QuantityMeasurementApp {
+	public static class Feet{
+		private final double value;
+
+		public Feet(double value) {
+			super();
+			this.value = value;
+		}
+		@Override
+		public boolean equals(Object o) {
+			Feet feet = (Feet) o;
+			
+			return Double.compare(this.value , feet.value) == 0;
+		}
+	}
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
