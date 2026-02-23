1aafec23ede295a343a758c0266518fe05451380
kapil kumar <kapil.kumar1_cs22@gla.ac.in>
2026-02-23 16:02:18 +0530

    [Kapil Kumar] Added UC3 - Generic Quantity Class For DRY Principles

---
+++ app/src/main/java/com/quantitymeasurementapp/app/Length.java
@@ -0,0 +1,51 @@
+package com.quantitymeasurementapp.app;
+
+import java.util.Objects;
+
+public class Length {
+  private final double value;
+  private final Unit unit;
+	
+	public Length(double value, Unit unit) {
+	super();
+	this.value = value;
+	this.unit = unit;
+}
+	public enum Unit{
+		FEET(12),
+		INCHES(1);
+		
+		private final double conversionFactor;
+		 
+		Unit(double conversionFactor) {
+			this.conversionFactor = conversionFactor;
+		}
+		
+		public double convertToBase(double value) {
+		return value * conversionFactor;
+		}
+	}	
+		
+    public double toInches() {
+			return unit.convertToBase(value);
+		}
+	@Override
+    public boolean equals(Object o) {
+		if(o == null)
+			return false;
+		if(this == o)
+			return true;
+		if(this.getClass() != o.getClass())
+			return false;
+		Length temp = (Length) o;
+		return Double.compare(this.toInches() , temp.toInches()) == 0;
+			
+
+	}
+	@Override
+    public int hashCode() {
+		return Objects.hash(toInches());
+	}
+	
+
+}
--- app/src/main/java/com/quantitymeasurementapp/app/QuantityMeasurementApp.java
+++ app/src/main/java/com/quantitymeasurementapp/app/QuantityMeasurementApp.java
@@ -5,65 +5,20 @@ package com.quantitymeasurementapp.app;
  */
 public class QuantityMeasurementApp {
 	
-	//***************UC1*************************
-	public static class Feet{
-		private final double value;
-
-		public Feet(double value) {
-			super();
-			this.value = value;
-		}
-		@Override
-		public boolean equals(Object o) {
-			if(o == null)
-		    return false;
-			
-			if (this.getClass() != o.getClass()) return false;
-			
-			Feet feet = (Feet) o;
-			
-			return Double.compare(this.value , feet.value) == 0;
-		}
-	}
-	//***************UC2*************************
-	public static class Inches{
-		private final double value;
-
-		public Inches(double value) {
-			super();
-			this.value = value;
-		}
-		@Override
-		public boolean equals(Object o) {
-			if(o == null)
-		    return false;
-			
-			if (this.getClass() != o.getClass()) return false;
-			
-			Inches inches = (Inches) o;
-			
-			return Double.compare(this.value , inches.value) == 0;
-		}
-	}
+//***********************************UC3*******************************************************
+	public static boolean compare(double value1 , Length.Unit unit1 , double value2, Length.Unit unit2 ) {
+		Length l1 = new Length(value1,unit1);
+		Length l2 = new Length(value2,unit2);
 		
-		public static boolean demonstrateFeetEquality(double d1 , double d2) {
-			Feet f1 = new Feet(d1);
-			Feet f2 = new Feet(d2);
-			return f1.equals(f2);
-		}
-		
-		public static boolean demonstrateInchesEquality(double d1 , double d2) {
-			Inches i1 = new Inches(d1);
-			Inches i2 = new Inches(d2);
-			return i1.equals(i2);
-		}
+		return l1.equals(l2);
 	
+	}
     public static void main(String[] args) {
-    	   System.out.println(demonstrateFeetEquality(1.0, 1.0));   // true
-    	     System.out.println(demonstrateFeetEquality(1.0, 2.0));   // false
+    	  System.out.println(compare(1, Length.Unit.FEET, 12, Length.Unit.INCHES));
 
-    	     System.out.println(demonstrateInchesEquality(5.0, 5.0)); // true
-    	     System.out.println(demonstrateInchesEquality(5.0, 7.0)); // false
+          System.out.println(compare(2, Length.Unit.FEET, 24, Length.Unit.INCHES));
+
+          System.out.println(compare(1, Length.Unit.FEET, 10, Length.Unit.INCHES));
         
     }
 }
